c74c866c303bf309a32d6ac3cfbe350389a0e3bb
Diego Ruiz <diegosantillan625@gmail.com>
2026-06-09 02:04:34 -0500

    Delete backend directory

--- backend/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .user import User
-from .category import Category
-from .post import Post
-from .comment import Comment
-from .report import Report
-
-__all__ = ["User", "Category", "Post", "Comment", "Report"]
--- backend/update_db.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-conn=sqlite3.connect('pincloud.db')
-c=conn.cursor()
-c.execute("UPDATE posts SET status='APPROVED'")
-conn.commit()
-print('DB Updated')
